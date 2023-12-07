715781e4cf6f1c953973d33724ed5e8e468fd43e
lkwslm <lkwslm@qq.com>
2023-12-07 21:00:47 +0800

    change

--- src/main/java/com/TwoJavaBeginner/FirstPackage/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.TwoJavaBeginner.FirstPackage;
-
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-}
--- src/main/java/com/TwoJavaBeginner/FirstPackage/Hellobegin.java
+++
@@ -1,7 +0,0 @@
-package com.TwoJavaBeginner.FirstPackage;
-
-public class Hellobegin {
-    public static void main(String[] args) {
-        System.out.println("Hello begin");
-    }
-}
